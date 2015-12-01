b9de2870117c72c6d71ba708ed49b9d5167a7a38
John Glancy <jeglancy@gmail.com>
2015-12-01 13:59:56 -0800

    feat: forumdb.py now uses a postgreSQL database

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,9 +3,8 @@
 # 
 
 import time
+import psycopg2
 
-## Database connection
-DB = []
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,9 +15,15 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    ## Database connection
+    DB = psycopg2.connect("dbname = forum")
+    ## Cursor creation
+    curs = DB.cursor()
+    curs.execute("SELECT content, time FROM posts ORDER BY time DESC")
+    posts = [{'content': str(row[0]), 'time': str(row[1])} for row in curs.fetchall()]
+    DB.close()
     return posts
+    
 
 ## Add a post to the database.
 def AddPost(content):
@@ -27,5 +32,12 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    ## Database connection
+    DB = psycopg2.connect("dbname = forum")
+    ## Cursor creation
+    curs = DB.cursor()
+    ## Very important, values must be passed as a tuple & single item tuple needs comma
+    ## Use DB API parameter passing rather than string substitution for security
+    curs.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
+    DB.commit()
+    DB.close()
