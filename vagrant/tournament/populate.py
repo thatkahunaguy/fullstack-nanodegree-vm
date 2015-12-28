4a63960bb248861e64061296803a8d9ac4ae3588
John Glancy <jeglancy@gmail.com>
2015-12-28 10:36:21 -0800

    refactor: update database schema for more flexibility - 1 participant per match record

--- vagrant/tournament/populate.py
+++ vagrant/tournament/populate.py
@@ -15,11 +15,28 @@ def populate():
     c = conn.cursor()
     c.execute("INSERT INTO players (player_name) values ('Joe Blow');")
     c.execute("INSERT INTO players (player_name) values ('Daisy Duke');")
+    c.execute("INSERT INTO tournaments (tournament_name) values ('US Open');")
     # get the id of the first player added so matches can be populated
     c.execute("SELECT player_id FROM players WHERE player_name = 'Joe Blow';")
     id = c.fetchone()
-    c.execute('''INSERT INTO matches (winner_id, loser_id, round)
-             values (%s, %s, 1);''',(id[0], id[0]+1))
+    # get the id of the first tournament added so matches can be populated
+    c.execute("SELECT tournament_id FROM tournaments WHERE tournament_name = 'US Open';")
+    tourney = c.fetchone()
+    # register players for the tournament
+    c.execute('''INSERT INTO tournament_registrations (player_id)
+             values (%s);''',(id[0],))
+    c.execute('''INSERT INTO tournament_registrations (player_id)
+             values (%s);''',(id[0]+1,))            
+    # create a match for the tournament
+    c.execute('''INSERT INTO matches (tournament_id, round)
+             values (%s, %s);''',(tourney[0], '1'))
+    c.execute("SELECT match_id FROM matches WHERE tournament_id = %s;",(tourney[0],))
+    match = c.fetchone()
+    # register the match winner and loser
+    c.execute('''INSERT INTO match_participants (match_id, player_id, match_result)
+             values (%s, %s, %s);''',(match[0], id[0], 'win'))
+    c.execute('''INSERT INTO match_participants (match_id, player_id, match_result)
+             values (%s, %s, %s);''',(match[0], id[0]+1, 'loss'))
     conn.commit() 
     conn.close()
     
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -13,68 +13,96 @@ CREATE DATABASE tournament;
 -- is this psql connect command something which is included generally?
 \c tournament;
 
+-- create an enumerated type for the match outcomes
+CREATE TYPE outcome AS ENUM ('win', 'loss', 'tie', 'bye', 'default');
+
 CREATE TABLE players (
     player_id serial PRIMARY KEY, 
     player_name text);
+
+-- Initial table design to add additional tournaments
+CREATE TABLE tournaments (
+    tournament_id serial PRIMARY KEY, 
+    tournament_name text);
     
--- Might be able to make primary key below a combo foreign of round & players?
+CREATE TABLE tournament_registrations (
+    tournament_id integer REFERENCES tournaments, 
+    player_id integer REFERENCES players);
+    
 CREATE TABLE matches (
      match_id serial PRIMARY KEY,
-     round integer,
-     winner_id integer REFERENCES players(player_id), 
-     loser_id integer REFERENCES players(player_id),
-     tie boolean);
---     tournament_id integer REFERENCES tournaments,
---     tie integer ???
-
--- initial table design to add additional tournaments in future
--- CREATE TABLE tournaments (
---     tournament_id serial PRIMARY KEY, 
---     tournament_name text);
---
--- CREATE TABLE tournament_registrations (
---     tournament_id integer REFERENCES tournaments, 
---     player_id integer REFERENCES players);
+     tournament_id integer REFERENCES tournaments,
+     round integer);
+ 
+ CREATE TABLE match_participants (
+     match_id integer REFERENCES matches,
+     player_id integer REFERENCES players,
+     match_result outcome);   
 
 
 -- Create views for win_record, loss_record, & matches played
 -- action: investigate whether this can be refactored into fewer views
+
 CREATE VIEW win_record AS
-    SELECT players.player_id AS player, players.player_name,
-           count(matches.winner_id) AS wins
-    FROM players LEFT JOIN matches
-        ON players.player_id = matches.winner_id  
+    SELECT player_id AS player, count(match_result) AS wins 
+    FROM match_participants          
+    WHERE match_result = 'win'  OR match_result = 'bye'
     GROUP BY player
     ORDER BY wins DESC;
+    
+CREATE VIEW loss_record AS
+    SELECT player_id AS player, count(match_result) AS losses 
+    FROM match_participants          
+    WHERE match_result = 'loss'
+    GROUP BY player
+    ORDER BY losses DESC;
 
--- technically not needed by the program as defined
--- CREATE VIEW loss_record AS
---     SELECT players.player_id AS player, 
---            count(matches.loser_id) AS losses
---     FROM players LEFT JOIN matches
---         ON players.player_id = matches.loser_id  
---     GROUP BY player;
+CREATE VIEW tie_record AS
+    SELECT player_id AS player, count(match_result) AS ties 
+    FROM match_participants          
+    WHERE match_result = 'tie'
+    GROUP BY player
+    ORDER BY ties DESC;
 
-
+-- might need to change this if we don't want to count defaults & byes as matches
 CREATE VIEW matches_played AS
-    SELECT players.player_id AS player, count(matches.match_id) AS matches
-    FROM players LEFT JOIN matches
-        ON (players.player_id = matches.winner_id) OR
-           (players.player_id = matches.loser_id)
+    SELECT player_id AS player, count(match_result) AS matches
+    FROM match_participants           
     GROUP BY player;
+    
+--  refactor investigation
+-- http://stackoverflow.com/questions/28436384/sql-counting-wins-and-losses
+-- select
+--     season, team,
+--     count(case when outcome = 'W' then 1 else null end) as wins,
+--     count(case when outcome = 'L' then 1 else null end) as losses
+-- from
+--     (
+--     select season, winning_team as team, 'W' as outcome from cbb.regular_season union all
+--     select season, losing_team as team, 'L' as outcome from cbb.regular_season
+--     ) as games
+-- group by season, team
+--
+-- need to add tournament functionality here
+CREATE VIEW standings AS
+    SELECT
+        player_id,
+        count(case when match_result = 'win' then 1 else NULL end) as wins,
+        count(case when match_result = 'loss' then 1 else NULL end) as losses,
+        count(case when match_result = 'tie' then 1 else NULL end) as ties,
+        count(match_result) as matches_played
+    FROM match_participants
+    GROUP BY player_id
+    ORDER BY wins DESC;
+        
 
--- WORKING ON TIES - just added a boolean but is this the best method?    
--- CREATE VIEW ties AS
---     SELECT players.player_id AS player, count(matches.match_id) AS matches
---     FROM players LEFT JOIN matches
---         ON (players.player_id = matches.winner_id) OR
---            (players.player_id = matches.loser_id)
---     GROUP BY player;
 
 CREATE VIEW match_record AS
-    SELECT win_record.player, win_record.player_name, win_record.wins as wins,  
-           matches_played.matches
-    FROM win_record 
---        JOIN loss_record ON win_record.player = loss_record.player
-        JOIN matches_played ON win_record.player = matches_played.player
+    SELECT players.player_id, players.player_name, win_record.wins as wins,  
+           loss_record.losses, tie_record.ties, matches_played.matches
+    FROM players 
+        LEFT JOIN win_record ON players.player_id = win_record.player
+        LEFT JOIN loss_record ON players.player_id = loss_record.player
+        LEFT JOIN tie_record ON players.player_id = tie_record.player
+        LEFT JOIN matches_played ON players.player_id = matches_played.player
     ORDER BY wins DESC;
